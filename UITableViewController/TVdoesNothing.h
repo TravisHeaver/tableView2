dde0c15af042e978904bc72e2eaea7666536a592
Travis Heaver <travisheaver@Traviss-MacBook-Pro.local>
2013-11-27 19:39:26 -0600

    added do nothing

---
+++ UITableViewController/TVdoesNothing.h
@@ -0,0 +1,13 @@
+//
+//  TVdoesNothing.h
+//  UITableViewController
+//
+//  Created by Travis Heaver on 11/27/13.
+//  Copyright (c) 2013 Travis Heaver. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TVdoesNothing : NSObject
+
+@end
---
+++ UITableViewController/TVdoesNothing.m
@@ -0,0 +1,13 @@
+//
+//  TVdoesNothing.m
+//  UITableViewController
+//
+//  Created by Travis Heaver on 11/27/13.
+//  Copyright (c) 2013 Travis Heaver. All rights reserved.
+//
+
+#import "TVdoesNothing.h"
+
+@implementation TVdoesNothing
+
+@end
